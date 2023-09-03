5d020c8b6e2a62c8c846834fb00ff71b6b75cd8e
rasheedawilson12 <rasheedawilson12@gmail.com>
2023-09-03 10:34:59 -0500

    Order Detail folder added with component and css

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,4 +1,4 @@
-import styles from "./CategoryList.css";
+import styles from "./CategoryList.module.css";
 
 export default function CategoryList({ categories, activeCat, setActiveCat }) {
   const cats = categories.map((cat) => (
